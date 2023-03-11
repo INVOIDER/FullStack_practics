0282486888308453a8579c3f5ca97c57267f0f97
INVOIDER <5261323@gmail.com>
2023-03-11 14:13:02 +0300

    2nd practic
    Deleted preinstalled React files that not needed yet.
    Made basic markup using import and export for css and js files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import Header from './components/Header'
+import classes from './components/style.module.css'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Test page for 1st practic
-        </a>
-      </header>
+        <body className={classes.body}>
+      <Header></Header>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga quaerat explicabo adipisci? Perferendis sapiente praesentium eaque corrupti, repellat explicabo sint fugit quod molestiae eveniet saepe voluptates ab dolorem incidunt dolorum?</p>
+      </body>
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import classes from './style.module.css'
+function Header() {
+    return (
+        <div className={classes.header}>
+            <h1>Это заголовок</h1>
+        </div>
+    );
+}
+export default Header;
